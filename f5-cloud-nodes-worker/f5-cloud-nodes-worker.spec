fe82a565b1339f67990361fafb17535d76153c97
Mike Levine <m.levine@f5.com>
2017-06-01 11:02:04 -0700

    First functional version

---
+++ f5-cloud-nodes-worker/f5-cloud-nodes-worker.spec
@@ -0,0 +1,41 @@
+Summary: F5 cloud nodes worker
+Name: f5-cloud-nodes-worker
+Version: 0.0.1
+Release: 1
+BuildArch: noarch
+Group: Development/Libraries
+License: Commercial
+Packager: F5 Networks <support@f5.com>
+
+%description
+REST worker for discovering nodes in a cloud environment
+
+%define BUILD_DIR %{_builddir}/%{name}-%{version}
+%define IAPP_DIR /var/config/rest/iapps/%{name}
+
+%prep
+rm -rf %{BUILD_DIR}
+mkdir -p %{BUILD_DIR}/node_modules
+cp -r -L %{main}/src/* %{BUILD_DIR}
+cp -r -L %{main}/node_modules %{BUILD_DIR}
+
+# update IAPP_NAME and VERSION in f5-iappslx-lib
+for fn in $(find %{BUILD_DIR}/nodejs -name \*.json -o -name \*.js)
+do
+    sed -i "s/<VERSION>//g" $fn
+    sed -i "s/<IAPP_NAME>/%{name}/g" $fn
+done
+
+%install
+
+# main
+rm -rf $RPM_BUILD_ROOT
+mkdir -p $RPM_BUILD_ROOT%{IAPP_DIR}
+cp -r %{BUILD_DIR}/* $RPM_BUILD_ROOT%{IAPP_DIR}
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+%files
+%defattr(-,root,root)
+%{IAPP_DIR}
---
+++ f5-cloud-nodes-worker/src/nodejs/nodesWorker.js
@@ -0,0 +1,184 @@
+/**
+ * Copyright 2017 F5 Networks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const logId = '[CloudNodesWorker]';
+
+/**
+ * A REST worker to query cloud providers for nodes based on query parameters
+ * @constructor
+ */
+function CloudNodesWorker() {}
+
+CloudNodesWorker.prototype.WORKER_URI_PATH = "shared/cloud/nodes";
+CloudNodesWorker.prototype.isPublic = true;
+
+/**
+ * Handles GET HTTP requests
+ *
+ * Looks for IPs by tag based on query parameters.
+ *
+ * Returns a response in the format
+ *
+ *    [
+ *        {
+ *            id: ID of owner
+ *            ip: IP address of node
+ *        }
+ *    ]
+ *
+ * @param {Object} restOperation
+ */
+CloudNodesWorker.prototype.onGet = function(restOperation) {
+    var query = restOperation.getUri().query;
+    var Provider;
+    var provider;
+    var providerPath;
+
+    this.logger.fine(logId, "onGet");
+
+    if (!query.cloud) {
+        restOperation.fail(new Error('cloud is a required parameter'));
+        return;
+    }
+    if (!query.memberTag) {
+        restOperation.fail(new Error('memberTag is a required parameter'));
+        return;
+    }
+    if (!query.memberAddressType) {
+        restOperation.fail(new Error('memberAddressType is a required parameter'));
+        return;
+    }
+
+    query.roleArn = query.roleArn || '';
+    query.externalId = query.externalId || '';
+    query.region = query.region || '';
+
+    this.logger.fine(logId, "using cloud", query.cloud);
+
+    this.logger.silly = this.logger.finest;
+    this.logger.debug = this.logger.fine;
+    this.logger.error = this.logger.severe;
+    this.logger.warn = this.logger.warning;
+    providerPath = '/config/cloud/' + query.cloud + '/node_modules/f5-cloud-libs/node_modules/f5-cloud-libs-' + query.cloud;
+    Provider = require(providerPath).provider;
+    provider = new Provider(
+        {
+            clOptions: {user: 'admin'},
+            logger: this.logger
+        }
+    );
+
+    this.logger.debug('Initializing cloud provider');
+    provider.init(
+        {
+            mgmtPort: query.mgmtPort || 443,
+            region: query.region.trim(),
+            tempCredentials: {
+                roleArn: query.roleArn.trim(),
+                externalId: query.externalId.trim(),
+            }
+        }
+    )
+    .then(function() {
+        this.logger.debug('Getting NICs');
+
+        var key;
+        var value;
+        var keyValue = query.memberTag.split('=');
+        var promises = [];
+
+        if (keyValue.length > 1) {
+            key = keyValue[0];
+            value = keyValue[1];
+        }
+        else {
+            value = keyValue[0];
+        }
+
+        this.logger.finest(logId, "key", key);
+        this.logger.finest(logId, "value", value);
+
+        promises.push(provider.getNicsByTag(
+            {
+                key: key,
+                value: value
+            }
+        ));
+        promises.push(provider.getVmsByTag(
+            {
+                key: key,
+                value: value
+            }
+        ));
+
+        return Promise.all(promises);
+    }.bind(this))
+    .then(function(responses) {
+        var nics = responses[0];
+        var vms = responses[1];
+        var nodes;
+
+        this.logger.finest('nics', JSON.stringify(nics));
+        this.logger.finest('vms', JSON.stringify(vms));
+
+        if (nics.length > 0) {
+            nodes = nics.reduce(function(result, nic) {
+                var node = getNode(nic, query);
+                if (node) {
+                    result.push(node);
+                }
+                return result;
+            }, []);
+        }
+
+        if (!nodes) {
+            this.logger.debug('no valid nics found, trying vms');
+            nodes = vms.reduce(function(result, vm) {
+                var node = getNode(vm, query);
+                if (node) {
+                    result.push(node);
+                }
+                return result;
+            }, []);
+        }
+
+        if (!nodes) {
+            this.logger.debug('no valid pool nodes found');
+        }
+        else {
+            restOperation.setContentType('application/json');
+            restOperation.setBody(nodes);
+            this.completeRestOperation(restOperation);
+        }
+    }.bind(this))
+    .catch(function(err) {
+        this.logger.severe('Error while getting nodes', err);
+        restOperation.fail(err);
+    }.bind(this));
+};
+
+var getNode = function(owner, query) {
+    var ip = query.memberAddressType.toLowerCase() === 'public' ? owner.ip.public : owner.ip.private;
+    if (ip) {
+        return {
+            id: owner.id + '-' + query.memberAddressType.toLowerCase(),
+            ip: ip
+        };
+    }
+};
+
+module.exports = CloudNodesWorker;
